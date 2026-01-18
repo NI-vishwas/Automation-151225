0d1efd9d5d1bcd820f06df223b21303708624938
vishwas singh <vishwasks.reach@gmail.com>
2026-01-18 20:53:42 +0530

    data and pattern matching

---
+++ 09-pattern_matching.py
@@ -0,0 +1,22 @@
+# Extract emails from emails.txt
+import re
+
+def extract_emails(text):
+    pat = "[a-z0-9.]+@[a-z]+\.[a-z]+" 
+    return re.findall(pat, text)
+
+def readfile(fname):
+    data = ''
+    try: 
+        with open(fname,'r') as fp:
+            data = fp.read()
+    except FileNotFoundError:
+        print('The file you are trying to read does not exist')
+
+    return data
+
+if __name__ == '__main__':
+    fname = input("Enter the filename to extract emails: ")
+    data = readfile(fname)
+    list_of_emails = extract_emails(data)
+    print(list_of_emails)
